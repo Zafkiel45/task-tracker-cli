8d6e5c53fc37b45d69fbb0413a83b524e5f7f441
zafkiel45 <zafkielc@gmail.com>
2025-03-06 21:50:39 -0300

    refatore: removed unecessary code

--- modules/backup.ts
+++ modules/backup.ts
@@ -1,7 +1,5 @@
 import path from "node:path";
 import fs from "node:fs/promises";
-// utils
-import { internalError } from "../util/internalError";
 
 const originalFilePath = path.join(import.meta.dirname, "tasks.json");
 const backupFolderPath = path.join(import.meta.dirname, "backup");
@@ -14,28 +12,20 @@ async function ensureBackupDirectoryExists() {
   try {
     await fs.access(backupFolderPath);
   } catch (err: any) {
-
-    if (typeof err.code !== "string") {
-      throw internalError("❌ the object is not a ErrnoException");
-    }
-
     if (err.code === "ENOENT") {
       await fs.mkdir(backupFolderPath);
       console.log(`✅ Diretório de backup criado com sucesso!`);
-    } else throw internalError("O objeto não é um ErrnoException");
-  }
-}
+    } else {
+      console.error("❌ O backup não foi realizado");
+    };
+  };
+};
 
 async function attemptCopyFile(src: string, dist: string, retries = 0) {
   try {
     await fs.copyFile(src, dist);
     console.log(`✅ Backup atualizado com sucesso!`);
   } catch (err: any) {
-
-    if (typeof err.code !== "string") {
-      throw internalError("❌ O object não é um ErrnoException");
-    };
-
     if (err.code === "EBUSY" && retries < MAX_RETRIES) {
       console.warn(
         `❌ Ocorreu um erro, tentando realizar o backup novamente em ${TIME_TO_RETRIES}`
@@ -45,9 +35,9 @@ async function attemptCopyFile(src: string, dist: string, retries = 0) {
       return attemptCopyFile(src, dist, retries + 1);
     } else {
       console.error(`❌ Não foi possível realizar o backup`, err);
-    }
-  }
-}
+    };
+  };
+};
 
 export async function performBackup() {
   try {
@@ -55,5 +45,5 @@ export async function performBackup() {
     await attemptCopyFile(originalFilePath, backupFilePath);
   } catch (err: any) {
     console.error(err.message.trim());
-  }
-}
+  };
+};
