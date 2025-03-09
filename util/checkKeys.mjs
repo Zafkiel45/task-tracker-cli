501626149ad7b444181d7fe23e19343c0c4fe50d
zafkiel45 <zafkielc@gmail.com>
2025-03-09 09:56:10 -0300

    fix: updated gitignore

--- util/checkKeys.mjs
+++ util/checkKeys.mjs
@@ -1,35 +1,36 @@
-import { argv } from 'node:process';
-import { readTasksFromJson } from '../modules/readTasksFromJson.ts';
+import { argv } from "node:process";
+import { readTasksFromJson } from "../modules/readTasksFromJson.ts";
 
 async function HandleReadFile() {
-    try {
-        const isTaskField = argv[2] !== 'configuration-task-field';
-        const isHelp = argv[2] !== 'help';
-        const isBackup = argv[2] !== 'backup';
-        const isNotification = argv[2] !== 'notification';
+  try {
+    const isTaskField = argv[2] !== "configuration-task-field";
+    const isHelp = argv[2] !== "help";
+    const isBackup = argv[2] !== "backup";
+    const isNotification = argv[2] !== "notification";
 
-        if(isTaskField && isHelp && isBackup && isNotification) {
-            const task = await readTasksFromJson();
-            const key = Object.keys(task[0]);
-    
-            const commandExist = key.find((item) => {
-                return item === argv[2];
-            });
-    
-            if(!commandExist) {
-                throw new Error(`
+    if (isTaskField && isHelp && isBackup && isNotification) {
+      const task = await readTasksFromJson();
+      const key = Object.keys(task[0]);
+
+      const commandExist = key.find((item) => {
+        return item === argv[2];
+      });
+
+      if (!commandExist) {
+        throw new Error(
+          `
                     Você está tentando acessar uma lista que não existe! 
                     Por favor, crie a lista antes para poder acessa-la. 
                     Caso tenha alguma dúvida, digite o comando "help" no 
                     terminal.
-                `.trim());
-            };
-        } 
-
-    } catch (err) {
-        console.error('O seguinte erro ocorreu:', err);
-        process.exit(1);
+                `.trim()
+        );
+      }
     }
-};
+  } catch (err) {
+    console.error("O seguinte erro ocorreu:", err);
+    process.exit(1);
+  }
+}
 
 HandleReadFile();
